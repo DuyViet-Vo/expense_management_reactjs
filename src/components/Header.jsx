bd78f0e6e5c54f6195effa2ae2ef470803971e19
VietVo-Rikai <viet.vo@rikai.technology>
2025-01-16 11:01:21 +0700

    chore: update button logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-  const navigate = useNavigate()
-  const handleLogout = () => {
-    // Xóa token khỏi localStorage (hoặc sessionStorage)
-    localStorage.removeItem("token");
-    // Thực hiện hành động sau khi đăng xuất, ví dụ: chuyển hướng
-    navigate("/login")
-  };
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
 
-  // Kiểm tra xem token có tồn tại hay không
-  const isLoggedIn = !!localStorage.getItem("token");
+  useEffect(() => {
+    const checkToken = () => setIsLoggedIn(!!localStorage.getItem("token"));
+    window.addEventListener("storage", checkToken);
+
+    return () => window.removeEventListener("storage", checkToken);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
 
   return (
     <header className="bg-gradient-to-r from-green-500 to-green-600 text-white py-8 px-4 shadow-lg">
